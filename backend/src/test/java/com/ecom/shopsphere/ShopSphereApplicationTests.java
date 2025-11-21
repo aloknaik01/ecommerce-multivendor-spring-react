ce03b945156c33e551a0cca7aef09ca83307bf95
aloknaik <aloknaik016261@gmail.com>
2025-11-21 14:34:42 +0530

    setup: initialized Spring Boot backend structure

---
+++ backend/src/test/java/com/ecom/shopsphere/ShopSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.shopsphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
