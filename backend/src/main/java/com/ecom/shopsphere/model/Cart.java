59794a62a811023d84a5e84cdd7c146e8ec1aab5
Alok kumar Naik <aloknaik016261@gmail.com>
2025-11-23 07:52:57 +0530

    add cart model

---
+++ backend/src/main/java/com/ecom/shopsphere/model/Cart.java
@@ -0,0 +1,28 @@
+package com.ecom.shopsphere.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+@OneToOne
+private User user;
+private Set<CartItem> cartItems = new HashSet<>();
+private double totalSellingPrice;
+private int toalItem;
+private int totalMrpPrice;
+private int discount;
+private String coupounCode;
+}
